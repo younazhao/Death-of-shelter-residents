b35704faf813775172f57f24ea7e633544f696c7
younazhao <youna.zhao@mail.utoronto.ca>
2024-09-24 00:25:21 -0400

    Read the simulated data from previous step Test for positive value only in the dataset Test for non NA value in the dataset

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,25 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Sanity check of the data
+# Author: Wen Han Zhao
+# Date: 24 September 2024
+# Contact: youna.zhao@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Need to have the simulated data
+# Any other information needed? None
 
 
 #### Workspace setup ####
+
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Test data ####
+data <- read.csv("data/raw_data/simulated.csv")
+
+# Since the data is randomly generated with poisson distribution, no negative number should be present in the data
+
+data$number_of_death |> min() <= 0
+
+# Since the data is randomly generated with poisson distribution, no NA data should be present in the data
+
+all(is.na(data$number_of_death))
