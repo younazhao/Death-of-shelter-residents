1279f4ff2b28703a4f23951dc3c8ef7595374871
younazhao <youna.zhao@mail.utoronto.ca>
2024-09-24 00:47:24 -0400

    Find the raw data wanted from Open Data Toronto Filter through the data resources wanted Save the raw data to "data/raw_data" folder

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,38 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Open data Toronto
+# Author: Wen Han Zhao
+# Date: 24 September 2024
+# Contact: youna.zhao@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Dataset found from Open data Toronto
+# Any other information needed?
 
 
 #### Workspace setup ####
+
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+# Search for the Deaths of Shelter Residents package from Open Data Toronto
+package <- search_packages("Deaths of Shelter Residents")
+
+# Get the package ID
+package_id <- package$id[1]
+
+# Get the package resource
+resource <- list_package_resources(package_id)
+
+# Filter for the datastore resources that we want 
+datastore_resources <- filter(resource, tolower(format) %in% c('csv'))
+
+# Get our csv data
+data <- filter(datastore_resources, row_number() == 1) |>
+  get_resource()
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(data, "data/raw_data/shelter_data.csv") 
 
          
