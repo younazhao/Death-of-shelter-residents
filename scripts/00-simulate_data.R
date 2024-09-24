d6bdf5bb191e01b4e505d77ad381d6354821dc76
younazhao <youna.zhao@mail.utoronto.ca>
2024-09-24 00:14:33 -0400

    Updated Simulation Data R Script Saved Simulation Data with seed 304 at "data/raw_data"

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,47 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates Data
+# Author: Wen Han Zhao
+# Date: 24 September 2024
+# Contact: youna.zhao@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
+
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
 
+set.seed(304)
 
+# Defining a Starting date & End Date
+
+start_date <- as.Date("2007-01-01")
+end_date <- as.Date("2024-08-31")
+
+# Number of dates we want to generate
+
+number_of_dates <- 200
+
+# Creating the simulated data frame
+
+data <- tibble(
+  date = as.Date(
+    runif(
+      n = number_of_dates,
+      min = start_date,
+      max = end_date
+    ),
+    origin = "1970-01-01"
+  ),
+  
+  number_of_death = rpois(n = number_of_dates, lambda = 10)
+)
+
+#### Write CSV to store the simulation data ####
+
+write_csv(data, file = "data/raw_data/simulated.csv")
 
